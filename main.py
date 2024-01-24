e98604bbbc98869362110cd9bcbc94f1e95b30eb
Mich1252 <mcbeccarelli01@gmail.com>
2024-01-23 20:04:32 -0700

    Create main.py
    created main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+
+This is the main class for the python game.
+
+For now, all code will be here, until we get to classes, then we can start divying up code among files. 
+
+
+
+'''
+
+
+def main():
+    print("start game")
+
+
+
+
+main()
